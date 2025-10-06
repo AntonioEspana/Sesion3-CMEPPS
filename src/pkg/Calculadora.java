5f4988657c82eedcdc762f0cd4fd9246709632c8
AntonioEspana <140651448+AntonioEspana@users.noreply.github.com>
2025-10-06 17:14:55 +0200

    Paso 2

---
+++ src/pkg/Calculadora.java
@@ -0,0 +1,21 @@
+package pkg;
+
+public class Calculadora {
+
+	public static int suma(int i, int j) {
+		return i + j;
+	}
+
+	public static int resta(int i, int j) {
+		return i - j;
+	}
+
+	public static int multiplica(int i, int j) {
+		return i * j;
+	}
+
+	public static int divide(int i, int j) {
+		return i/j;
+	}
+
+}
